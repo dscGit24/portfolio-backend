800b66637b8498fa98f82708dbd069d732594aa6
Disha Chotai <chotaidisha24@gmail.com>
2026-06-10 01:05:11 +0530

    Portfolio backend ready for deployment

--- src/main/java/com/disha/portfolio/controller/ContactController.java
+++ src/main/java/com/disha/portfolio/controller/ContactController.java
@@ -1,8 +1,9 @@
 package com.disha.portfolio.controller;
 
 import com.disha.portfolio.dto.ContactRequest;
+import com.disha.portfolio.dto.ContactResponse;
 import com.disha.portfolio.service.ContactService;
-import com.disha.portfolio.service.EmailService;
+//import com.disha.portfolio.service.EmailService;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/contact")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class ContactController {
 
 //    private final EmailService emailService;
@@ -28,8 +28,13 @@ public class ContactController {
     private final ContactService service;
 
     @PostMapping
-    public ResponseEntity<String> submitContact(@Valid @RequestBody ContactRequest request) throws MessagingException {
+    public ResponseEntity<ContactResponse> submitContact(@Valid @RequestBody ContactRequest request) throws MessagingException {
         service.saveContact(request);
-        return  ResponseEntity.ok("Message Submitted Successfully..");
+        return ResponseEntity.ok(
+                new ContactResponse(
+                        "success",
+                        "Message Submitted Successfully.."
+                )
+        );
     }
 }
