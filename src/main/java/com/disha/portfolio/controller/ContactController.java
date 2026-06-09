4ede7dc4156e9ee37ef386f5b06b3d1611a4e07c
Disha Chotai <chotaidisha24@gmail.com>
2026-06-09 19:05:09 +0530

    Frontend and Backend Connected

--- src/main/java/com/disha/portfolio/controller/ContactController.java
+++ src/main/java/com/disha/portfolio/controller/ContactController.java
@@ -7,14 +7,12 @@ import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contact")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class ContactController {
 
 //    private final EmailService emailService;
